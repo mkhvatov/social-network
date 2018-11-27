561405f12ff8883f075c5c10871f44ea1934045e
Matvey Khvatov <matveykhvatov@gmail.com>
2018-11-28 00:38:52 +0300

    basic factory structure

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask
+# from flask.ext.mongoengine import MongoEngine
+from flask_mongoengine import MongoEngine
+
+db = MongoEngine()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('settings.py')
+
+    db.init_app(app)
+
+    from user.views import user_app
+    app.register_blueprint(user_app)
+
+    return app
---
+++ user/views.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+user_app = Blueprint('user_app', __name__)
+
+
+@user_app.route('/login')
+def login():
+    return 'User login'
