9f2fd7851334f48eb30e557977b6777b6e07c8f8
Matvey Khvatov <matveykhvatov@gmail.com>
2019-01-29 23:45:11 +0300

    user profile image functionality

--- user/models.py
+++ user/models.py
@@ -1,7 +1,10 @@
 from mongoengine import signals
+from flask import url_for
+import os
 
 from application import db
 from utilities.common import utc_now_timestamp as now
+from settings import STATIC_IMAGE_URL as IMAGE_URL
 
 
 class User(db.Document):
@@ -14,11 +17,15 @@ class User(db.Document):
     bio = db.StringField(db_field="b", max_length=160)
     email_confirmed = db.BooleanField(db_field="ecf", default=False)
     change_configuration = db.DictField(db_field="cc")
+    profile_image = db.StringField(db_field="i", default=None)
 
     @classmethod
     def pre_save(cls, sender, document, **kwargs):
         document.username = document.username.lower()
         document.email = document.email.lower()
+
+    def profile_imgsrc(self, size):
+        return os.path.join(IMAGE_URL, 'user', '%s.%s.%s.png' % self.id, self.profile_image, size)
 
     meta = {
         'indexes': ['username', 'email', '-created'],
---
+++ utilities/imaging.py
@@ -0,0 +1,38 @@
+from wand.image import Image
+import os
+
+from utilities.common import utc_now_timestamp as now
+from settings import UPLOAD_FOLDER
+
+
+def thumbnail_process(file, content_type, content_id, sizes=[('sm', 50), ('lg', 75), ('xlg', 200)]):
+    img_id = now()
+    filename_template = content_id + '.%s.%s.png'
+
+    # original
+    with Image(filename=file) as img:
+        crop_center(img)
+        img.format = 'png'
+        img.save(filename=os.path.join(UPLOAD_FOLDER, content_type, filename_template % (img_id, 'raw')))
+
+    # sizes
+    for (name, size) in sizes:
+        with Image(filename=file) as img:
+            crop_center(img)
+            img.sample(size, size)
+            img.format = 'png'
+            img.save(filename=os.path.join(UPLOAD_FOLDER, content_type, filename_template % (img_id, name)))
+
+    os.remove(file)
+    return img_id
+
+
+def crop_center(image):
+    width_less_than_height = 1 > image.width / image.height
+    square_side = image.width if width_less_than_height else image.height
+    image.crop(
+        left=int((image.width - square_side) / 2),
+        top=int((image.height - square_side) / 2),
+        width=int(square_side),
+        height=int(square_side)
+    )
