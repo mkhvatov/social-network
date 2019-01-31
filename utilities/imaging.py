9475e67aec0f5c5af4fd0f947760165b87927fa1
Matvey Khvatov <matveykhvatov@gmail.com>
2019-01-31 22:10:03 +0300

    added AWS hosting for imaging

--- utilities/imaging.py
+++ utilities/imaging.py
@@ -1,8 +1,11 @@
-from wand.image import Image
 import os
 
+from wand.image import Image
+import boto3
+from boto3.s3.transfer import S3Transfer
+
 from utilities.common import utc_now_timestamp as now
-from settings import UPLOAD_FOLDER
+from settings import UPLOAD_FOLDER, AWS_BUCKET
 
 
 def thumbnail_process(file, content_type, content_id, sizes=[("sm", 50), ("lg", 75), ("xlg", 200)]):
@@ -22,6 +25,26 @@ def thumbnail_process(file, content_type, content_id, sizes=[("sm", 50), ("lg", 
             img.sample(size, size)
             img.format = 'png'
             img.save(filename=os.path.join(UPLOAD_FOLDER, content_type, filename_template % (image_id, name)))
+
+    if AWS_BUCKET:
+        s3 = boto3.client('s3')
+        transfer = S3Transfer(s3)
+        transfer.upload_file(
+            os.path.join(UPLOAD_FOLDER, content_type, filename_template % (image_id, 'raw')),
+            AWS_BUCKET,
+            os.path.join(content_type, filename_template % (image_id, 'raw')),
+            extra_args={'ACL': 'public-read', 'ContentType': 'image/png'}
+        )
+        os.remove(os.path.join(UPLOAD_FOLDER, content_type, filename_template % (image_id, 'raw')))
+
+        for (name, size) in sizes:
+            transfer.upload_file(
+                os.path.join(UPLOAD_FOLDER, content_type, filename_template % (image_id, name)),
+                AWS_BUCKET,
+                os.path.join(content_type, filename_template % (image_id, name)),
+                extra_args={'ACL': 'public-read', 'ContentType': 'image/png'}
+            )
+            os.remove(os.path.join(UPLOAD_FOLDER, content_type, filename_template % (image_id, name)))
 
     os.remove(file)
     return image_id
