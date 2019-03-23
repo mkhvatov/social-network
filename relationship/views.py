50bdc9a945c9eac31a806ba32920d8e5dee0a168
Matvey Khvatov <matveykhvatov@gmail.com>
2019-03-23 08:49:36 +0300

    add relationship module, relationship model

---
+++ relationship/views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+
+relationship_app = Blueprint('relationship_app', __name__)
+
+
+@relationship_app.route('/add_friend', methods=['GET', 'POST'])
+def add_friend():
+    return 'Add_Friend'
